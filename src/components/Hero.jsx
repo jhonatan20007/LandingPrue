ff5c2956eaccb825c8dc00407161aad6934cc73a
jbairont <jbairont@gmail.com>
2024-05-28 11:07:38 -0500

    ajuste

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,25 +23,29 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Explora las posibilidades de la
+            <span className="text-blue-700">
+              &nbsp; transaformación &nbsp;digital{" "}
+            </span>
+            {/* &nbsp; {` `} */}
             <span className="inline-block relative">
-              Brainwave{" "}
+              Ipaid{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
-                width={624}
-                height={28}
+                width={724}
+                height={48}
                 alt="Curve"
               />
             </span>
           </h1>
-          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-1/80 lg:mb-8">
+            Simplifica tus pagos en un mundo en movimiento. Usado en tiendas,
+            bares, hoteles y gasolineras, iPaid hace que las transacciones sean
+            rápidas y sin complicaciones. Únete a nosotros y transforma tu forma
+            de pagar.
           </p>
-          <Button href="/pricing" white>
-            Get started
-          </Button>
+          <Button href="#pricing">Get started</Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
