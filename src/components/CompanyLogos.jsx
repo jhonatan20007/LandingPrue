85430504554ca49fa67fb7b3f1bc98faf2dc015f
unknown <jhon.t@2nv.co>
2025-01-22 16:50:47 -0500

    mejoras visuales

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,21 +1,24 @@
 import { companyLogos } from "../constants";
 
 const CompanyLogos = ({ className }) => {
+    //  const duplicateLogos = [...companyLogos, ...companyLogos];
   return (
     <div className={className}>
       <h5 className="tagline mb-6 text-center text-n-1/80">
         Ayuda a impulsar a las empresas para pagos rápidos y seguros
       </h5>
-      <ul className="flex">
-        {companyLogos.map((logo, index) => (
+      <div className="carousel">
+      <ul className="flex carousel-track">
+        {companyLogos.concat(companyLogos).map((logo, index) => (
           <li
             className="flex items-center justify-center flex-1 h-[8.5rem]"
             key={index}
           >
-            <img src={logo} width={134} height={28} alt={logo} />
+            <img src={logo} width={134} height={28} alt={`logo-${index}`} />
           </li>
         ))}
       </ul>
+    </div>
     </div>
   );
 };
